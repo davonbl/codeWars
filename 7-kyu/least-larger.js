d2381487190bea908fa61e84e2461d3c000d6591
davonbl-bl <davonbl@gmail.com>
2023-09-04 17:01:44 -0400

    completed a 7 kyu coding challenge

---
+++ 7-kyu/least-larger.js
@@ -0,0 +1,7 @@
+function leastLarger(a,i) {
+    let newA = [];
+    a.forEach((ele, index,array) => {
+      ele > array[i]? newA.push({value: ele, index: index}): null;
+    })
+    return newA.length > 0? newA.sort( (a, b) => a.value - b.value )[0].index: -1;
+  }
